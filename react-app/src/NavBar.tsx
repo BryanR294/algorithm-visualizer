0591cc851bf2b38e71fe84ba135426f201133177
BryanR294 <bryanareval@gmail.com>
2025-02-14 19:56:59 -0500

    Seperated Main App into two components; NavBar functionality not yet added.

---
+++ react-app/src/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return ( 
+        <ul>
+            <li> Array Sort </li>
+            <li> Graph Traversal </li>
+            <li> More Info </li>
+            <li> Github </li>
+        </ul>
+     );
+}
+ 
+export default NavBar;
